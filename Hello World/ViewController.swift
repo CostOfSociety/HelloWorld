0b79f8872ea1d4506c194834e4d0612ff43062ba
CostOfSociety <48133616+CostOfSociety@users.noreply.github.com>
2022-09-13 23:34:28 +0300

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Святослав on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
